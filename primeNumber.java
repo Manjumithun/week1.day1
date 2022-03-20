a75194e617d46a0d828a22b2a6f83f2de8c7a710
Manjumithun <101967918+Manjumithun@users.noreply.github.com>
2022-03-20 13:49:40 +0530

    Add files via upload

---
+++ primeNumber.java
@@ -0,0 +1,30 @@
+package week1.day1;
+
+public class primeNumber {
+
+	public static void main(String[] args) {
+		
+		int num=3;
+		int count=0;
+		
+		for(int i=2;i<num;i++)
+		{
+			if(num%i==0)
+			{
+				count++;
+				break;
+			}
+		}
+		if(count==0)
+		{
+			System.out.println("Num is a prime number");
+		}
+		else
+		{
+			System.out.println("Num is not a prime number");
+		}
+		
+
+	}
+
+}
